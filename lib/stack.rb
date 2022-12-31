e2c13324cb517870f5c050bfcf41a473021bd61e
Ryan DeLong <rdelong111@gmail.com>
2022-12-31 16:33:01 -0600

    commit tree d7f040740af28db644cf8bfe2957e2a9ea8f599b

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,18 @@
 # your code here
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def push(value)
+    @stack.push(value)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack[@stack.size - 1]
+  end
+end
